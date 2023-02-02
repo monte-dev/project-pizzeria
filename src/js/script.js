5fba37f587095f136431dfe0edc791917f553765
monte-dev <119732972+monte-dev@users.noreply.github.com>
2023-02-02 12:22:59 +0000

    Add render products on page with renderInMenu

--- src/js/script.js
+++ src/js/script.js
@@ -52,7 +52,24 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  
+
   const app = {
+    initMenu: function(){
+      const thisApp = this;
+      console.log('thisApp.data: ', thisApp.data);
+      
+      for(let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+        
+      }
+    },
+
+    initData: function() {
+      const thisApp = this;
+      thisApp.data = dataSource;
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,8 +77,36 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      this.initData();
+      thisApp.initMenu();
     },
   };
+
+  class Product{
+    constructor(id, data){
+      const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+      console.log('new Product: ', thisProduct);
+      
+    }
+    renderInMenu(){
+      const thisProduct = this;
+
+      /* generate HTML based on template */
+      const generateHTML = templates.menuProduct(thisProduct.data);
+      
+      /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generateHTML);
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
+    }
+  }
 
   app.init();
 }
