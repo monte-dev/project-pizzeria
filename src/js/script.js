0e69ab95bb9a0800624a00954bd8878191722d24
monte-dev <119732972+monte-dev@users.noreply.github.com>
2023-02-03 10:29:22 +0000

    Add accordion to product menu list

--- src/js/script.js
+++ src/js/script.js
@@ -91,7 +91,7 @@
 
       thisProduct.renderInMenu();
       console.log('new Product: ', thisProduct);
-      
+      thisProduct.initAccordion();
     }
     renderInMenu(){
       const thisProduct = this;
@@ -106,7 +106,28 @@
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
     }
+    initAccordion(){
+      const thisProduct = this;
+      
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+      
+        /* prevent default action for event */
+        event.preventDefault();
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
+        
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if(activeProduct && activeProduct !== thisProduct.element) {
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+      });  
+    } 
   }
-
   app.init();
 }
