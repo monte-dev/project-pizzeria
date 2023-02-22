34aa3fb5b96d72967a3dc178c294f0754fa79738
monte-dev <119732972+monte-dev@users.noreply.github.com>
2023-02-22 16:57:44 +0000

    ADD get booking data fn fix app.json settings.js url bugs

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,4 @@
-import {templates, select} from '../settings.js';
+import {templates, settings, select} from '../settings.js';
 import utils from '../utils.js';
 import AmountWidget from '../components/AmountWidget.js';
 import DatePicker from '../components/DatePicker.js';
@@ -11,7 +11,49 @@ class Booking {
   
     thisBooking.render(element);
     thisBooking.initWidgets();
+    thisBooking.getData();
   }
+
+  getData(){
+    const thisBooking = this;
+
+    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+
+    const params = {
+      bookings: [startDateParam, endDateParam],
+      eventsCurrent: [settings.db.notRepeatParam ,startDateParam, endDateParam,],
+      eventsRepeat: [settings.db.repeatParam, endDateParam,],
+    };
+
+
+    const urls = {
+      bookings:       settings.db.url + '/' + settings.db.bookings + '?' + params.bookings.join('&'),
+      eventsCurrent: settings.db.url + '/' + settings.db.events + '?' + params.eventsCurrent.join('&'),
+      eventsRepeat:  settings.db.url + '/' + settings.db.events + '?' + params.eventsRepeat.join('&'),
+    };
+    
+    Promise.all([
+      fetch(urls.bookings),
+      fetch(urls.eventsCurrent),
+      fetch(urls.eventsRepeat),
+    ])
+      .then(function(allResponses){
+        const bookingsResponse = allResponses[0];
+        const eventsCurrentResponse = allResponses[1];
+        const eventsRepeatResponse = allResponses[2];
+
+        return Promise.all([
+          bookingsResponse.json(),
+          eventsCurrentResponse.json(),
+          eventsRepeatResponse.json(),
+        ]);
+      })
+      .then(function([bookings, eventsCurrent, eventsRepeat]){
+        console.log(bookings);
+        console.log(eventsCurrent);
+        console.log(eventsRepeat);
+      });}
   render(element){
     const thisBooking = this;
     // generate html for booking container from template
@@ -25,6 +67,7 @@ class Booking {
     thisBooking.dom.datePicker = document.querySelector(select.widgets.amount.datePicker.wrapper);
     thisBooking.dom.hourPicker = document.querySelector(select.widgets.amount.hourPicker.wrapper);
   }
+
   initWidgets(){
     const thisBooking = this;
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
